ae795e04b16f1de6ab1c778403e5e866e7713962
Hathaitorn Rojnirun <31265604+hathaitornr@users.noreply.github.com>
2018-01-20 20:24:54 -0500

    Update MainActivity.java

--- app/src/main/java/com/example/android/quidditchscorekeeper/MainActivity.java
+++ app/src/main/java/com/example/android/quidditchscorekeeper/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 
     @Override
