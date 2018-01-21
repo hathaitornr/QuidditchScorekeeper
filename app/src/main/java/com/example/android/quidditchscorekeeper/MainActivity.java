7a06e0822f7aa98b9cb6242c7d1dfec03dab1fc3
hathaitornr <hathaitornr@gmail.com>
2018-01-21 01:13:01 -0500

    Use switch statement to select between button IDs and prevent screen rotation

--- app/src/main/java/com/example/android/quidditchscorekeeper/MainActivity.java
+++ app/src/main/java/com/example/android/quidditchscorekeeper/MainActivity.java
@@ -1,21 +1,43 @@
 package com.example.android.quidditchscorekeeper;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     int gScore = 0;
     int sScore = 0;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        Button b1 = (Button) findViewById(R.id.addThirtyForGryffindor);
+        b1.setOnClickListener(this);
+
+        Button b2 = (Button) findViewById(R.id.addTenForGryffindor);
+        b2.setOnClickListener(this);
+
+        Button b3 = (Button) findViewById(R.id.addThirtyForSlytherin);
+        b3.setOnClickListener(this);
+
+        Button b4 = (Button) findViewById(R.id.addTenForSlytherin);
+        b4.setOnClickListener(this);
+
+        Button reset = (Button) findViewById(R.id.reset);
+        reset.setOnClickListener(this);
     }
 
     @Override
@@ -41,42 +63,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * Increase the score for Gryffindor.
-     */
-    public void addThirtyForGryffindor(View v) {
-        gScore = gScore + 30;
-        displayForGryffindor(gScore);
-    }
-    
-    public void addTenForGryffindor(View v) {
-        gScore = gScore + 10;
-        displayForGryffindor(gScore);
-    }
-    
-    /**
-     * Increase the score for Slytherin.
-     */
-    public void addThirtyForSlytherin(View v) {
-        sScore = sScore + 30;
-        displayForSlytherin(sScore);
-    }
-
-    public void addTenForSlytherin(View v) {
-        sScore = sScore + 10;
-        displayForSlytherin(sScore);
-    }
-
-    /**
-     * Resets the score for both teams back to 0.
-     */
-    public void resetScore(View v) {
-        gScore = 0;
-        sScore = 0;
-        displayForGryffindor(gScore);
-        displayForSlytherin(sScore);
-    }
-
-    /**
      * Displays the given score for Gryffindor.
      */
     public void displayForGryffindor(int score) {
@@ -90,6 +76,53 @@ public class MainActivity extends AppCompatActivity {
     public void displayForSlytherin(int score) {
         TextView scoreView = (TextView) findViewById(R.id.s_score);
         scoreView.setText(String.valueOf(score));
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch(v.getId()){
+
+            /**
+             * Increase the score for Gryffindor.
+             */
+
+            case R.id.addThirtyForGryffindor:
+                gScore = gScore + 30;
+                displayForGryffindor(gScore);
+                break;
+
+            case R.id.addTenForGryffindor:
+                gScore = gScore + 10;
+                displayForGryffindor(gScore);
+                break;
+
+            /**
+             * Increase the score for Slytherin.
+             */
+
+            case R.id.addThirtyForSlytherin:
+                sScore = sScore + 30;
+                displayForSlytherin(sScore);
+                break;
+
+            case R.id.addTenForSlytherin:
+                sScore = sScore + 10;
+                displayForSlytherin(sScore);
+                break;
+
+            /**
+             * Resets the score for both teams back to 0.
+             */
+
+            case R.id.reset:
+                gScore = 0;
+                sScore = 0;
+                displayForGryffindor(gScore);
+                displayForSlytherin(sScore);
+                break;
+
+        }
     }
 }
 
